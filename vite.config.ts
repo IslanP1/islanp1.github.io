540865ee4c413b9ef4ca67f74d448ad39982a25e
IslanP1 <islanpereiradeoliveira@gmail.com>
2023-11-16 11:16:01 -0300

    desfazendo configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/islanp1.github.io",
+  // base: "/islanp1.github.io",
 })
