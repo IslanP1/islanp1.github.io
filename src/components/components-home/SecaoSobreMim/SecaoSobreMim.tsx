40771545374ece8e1b7391e7c4f17e4edbf9d8fc
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-01-17 15:04:12 -0300

    refactor: mudando para português e retirando foto

--- src/components/components-home/SecaoSobreMim/SecaoSobreMim.tsx
+++ src/components/components-home/SecaoSobreMim/SecaoSobreMim.tsx
@@ -6,16 +6,18 @@ function SecaoSobreMim() {
   return (
     <section className={module.secaoSobreMim} id="about">
       <div className={module.sobre}>
-        <h1 id={module.tituloPrincipal}>Hi, I'm Islan</h1>
+        <h1 id={module.tituloPrincipal}>Oi, eu sou Islan</h1>
         <Conteudo
           titulo="Full-stack developer and innovation enthusiast"
-          texto="As a Junior Full-Stack Developer, I bring proficiency in Python,
-            Django, and front-end technologies such as JavaScript, HTML, and CSS.
-            My expertise extends to React, React Native, and Next.js for building
-            engaging and responsive applications. Additionally, I possess a
-            foundational understanding of C for low-level programming. Eager to
-            contribute and learn, I am ready to bring my skills to your
-            development projects."
+          texto="Sou um Desenvolvedor Júnior Full-Stack com experiência em tecnologias modernas e paixão por criar soluções 
+          eficientes. Tenho proficiência em Python e Django para o desenvolvimento back-end, além de experiência em Java e Node.js 
+          com Express para a construção de aplicações escaláveis e robustas. No front-end, utilizo React e React Native para criar 
+          interfaces interativas e responsivas. Minha base em C oferece uma compreensão sólida de programação de baixo nível, 
+          complementando minha habilidade em resolver problemas e otimizar processos. Também possuo experiência em bancos de dados
+          SQL e NoSQL, garantindo a modelagem e gestão eficiente de dados para atender a diferentes necessidades de aplicações. 
+          Estou constantemente em busca de novos desafios e oportunidades para aprender, crescer e contribuir com projetos 
+          inovadores. Com um espírito colaborativo e comprometido, estou pronto para transformar ideias em realidade e agregar 
+          valor ao seu projeto."
         />
 
         <div className={module.botoesSecaoSobreMim}>
